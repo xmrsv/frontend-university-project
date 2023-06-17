221ae236a892235ebbb92da867bdd8c7804686a5
xmrsv <88881293+xmrsv@users.noreply.github.com>
2023-06-16 23:46:00 -0500

    Tipado modificado

--- src/utils/integrations.js
+++
@@ -1,4 +0,0 @@
-// Enviar mensaje por WhatsApp usando api.whatsapp.com/numero?text=mensaje
-export function sendWhatsapp(phone, message) {
-    window.open(`https://api.whatsapp.com/send?phone=${phone}&text=${message}`, '_blank');
-}

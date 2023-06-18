a89aae27163a6c7d7c9c8bad18c7b8948831110c
mgrmars <88881293+mgrmars@users.noreply.github.com>
2023-06-18 02:30:12 -0500

    probando ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,5 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  /*
-  output: "server",
-  adapter: netlify({
-    builders: true,
-  }),
-  */
+  output: "server"
 });

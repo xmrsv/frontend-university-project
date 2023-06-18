f800b949bb941e14ede2688073427ba3925fd8c1
mgrmars <88881293+mgrmars@users.noreply.github.com>
2023-06-17 23:59:00 -0500

    builders for CDN (faster loading times)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify({
+    builders: true,
+  }),
 });

827fb5dd15a512c7293e65180d18844babb59ab2
mgrmars <88881293+mgrmars@users.noreply.github.com>
2023-06-25 22:20:32 -0500

    refactors

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
+  compressHTML: true,
   output: 'server',
   adapter: netlify({
     dist: new URL('./dist/', import.meta.url)
--- src/scripts/utils/localStorage.ts
+++ src/scripts/utils/localStorage.ts
@@ -1,20 +1,22 @@
 // Agrega un item al localstorage, key es el nombre del item y value es lo que contiene
 export function addLocalStorage(key: string, value: any): void {
-    let items: any = getLocalStorage(key);
+    const items: any = getLocalStorage(key);
     items.push(value);
     localStorage.setItem(key, JSON.stringify(items));
 }
 
 // Obtiene los items del localstorage
 export function getLocalStorage(key: string): Object {
-    return JSON.parse(localStorage.getItem(key)) || [];
+    const items = localStorage.getItem(key);
+    const parsedItems = JSON.parse(items || '[]');
+    return parsedItems;
 }
 
 // Elimina un item del localstorage
-export function deleteLocalStorage(key: string, value: any): void {
-    // De tipo any porque de tipo Object no funciona :(
-    let items: any = getLocalStorage(key); 
-    let newItems = items.filter(
+export function deleteLocalStorage(key: string, value: string): void {
+    // De tipo any porque de tipo Object no funciona
+    const items: any = getLocalStorage(key); 
+    const newItems = items.filter(
         (product: any) => product !== value);
     localStorage.setItem(key, JSON.stringify(newItems));
 }
